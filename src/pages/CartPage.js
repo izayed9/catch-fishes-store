0144286d8852c3087a1423ba887983d321d84cb6
Zayed <izayed9@gmail.com>
2021-02-02 22:37:53 +0600

    Create a db.json file with some data

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CartPage = () => {
     return (
         <div>
-            <h1>This Your Cart</h1>
+        <h1>This Your Cart</h1>
         <p>hello</p>
         </div>
     )
